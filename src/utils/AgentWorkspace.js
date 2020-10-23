a8808899448fdcf79330ebd288c769670eccddb0
DarryllRobinson <darryll@stillproud.com>
2020-10-23 14:25:44 +1000

    Fixed logout push to login page

--- src/utils/AgentWorkspace.js
+++ src/utils/AgentWorkspace.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
 import ComponentRoutes from './ComponentRoutes';
 //import NavBar from 'views/Components/NavBar';
 import Container from '@material-ui/core/Container';
@@ -8,8 +7,8 @@ import { history } from 'services/history';
 export default function AgentWorkspace(props) {
   console.log('AgentWorkspace props: ', props);
   return (
-      <Container style={{ marginTop: "80px" }}>
-        <ComponentRoutes history={history}/>
-      </Container>
+    <Container style={{ marginTop: "80px" }}>
+      <ComponentRoutes history={history}/>
+    </Container>
   )
 }
